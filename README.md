0c429e74c8a0fb23e90d32784e89d4c57e52eb7c
Paulo Jonas <pjonas21@gmail.com>
2023-03-12 11:19:22 -0300

    segundo dia de desafio

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Aqui estao alguimas tecnologias utilizadas no desenvolvimento do projeto:
 
 - Java
 
-### 1 dia do desafio
+### 1º dia do desafio
 
 No primeiro dia do desafio, foi pedido que utilizando a linguagem Java, acessassemos a API do IMDB, para poder mostar na linha de comando os Top 250 Filmes.
 
@@ -21,4 +21,18 @@ No primeiro dia do desafio, foi pedido que utilizando a linguagem Java, acessass
 
 ![Imagem da tela do codigo da aplicacao](assets/filmes-java-dia-1.png)
 
+### 2º dia do desafio
 
+No segundo dia do desafio, foi pedido que após receber o `json` com a resposta da chamada da API, devíamos parsear a resposta de forma a obter o título, a url da imagem, ano e imdb rating de cada filme.
+
+#### Passos realizados
+- Foi criado o método `private static String[] parse(String json){}`, com a finalidade de parsear o json recebido da API, retornando um Array de todos os filmes;
+- Utilizando `regex` com o padrão `".*\\[(.*)\\].*"`, com as classes `Matcher` e `Pattern`, pude percorrer a resposta recebida, com a finalidade de eliminar os `[]`, para isolar a string do json;
+- Para eliminar as `{}`, primeiro foi definido o padrão `"\\},\\{"`, percorrendo cada item do Array, utilizando os métodos `group`, `split` e `substring`;
+- Criado o método `private static List<String> parseAtrib(String[] moviesArray, int pos){}` para parsear cada atributo presente no array, onde recebe o array de filmes e a posição específica do atributo desejado;
+- A partir do método `parseAtrib` é possível pegar qualquer atributo do array, apenas criando um método específico para cada um que desejar;
+
+
+![Imagem da tela do codigo da aplicacao](assets/filmes-java-dia-2-0.png)
+![Imagem da tela do codigo da aplicacao](assets/filmes-java-dia-2-1.png)
+![Imagem da tela do codigo da aplicacao](assets/filmes-java-dia-2-2.png)
--- src/App.java
+++ src/App.java
@@ -3,11 +3,22 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+// dia 1 - acessar os dados da API e mostrar na linha de comando
+// dia 2 - parsear a resposta obtida e extrair o titulo e a URL da imagem de cada filme
 public class App {
+    
     public static void main(String[] args) throws Exception {      
-
-        String endApi = "https://imdb-api.com/en/API/Top250Movies/apiKey";
+        String key = "k_61y8j4ml";
+        String endApi = "https://imdb-api.com/en/API/Top250Movies/" + key;
         URI url = new URI(endApi);
 
         HttpRequest req = HttpRequest.newBuilder()
@@ -18,7 +29,52 @@ public class App {
         HttpClient client = HttpClient.newHttpClient();
 
         HttpResponse<String> resp = client.send(req, BodyHandlers.ofString());
+        String json = resp.body();
 
-        System.out.println(resp.body());
+        String[] moviesArray = parse(json);
+
+        List<String> titles = parseTitles(moviesArray);
+        for (String title : titles){
+            System.out.println("Titulo: " + title);
+        }
+
+        List<String> urlImages = parseUrlImages(moviesArray);
+        for (String urlIm : urlImages){
+            System.out.println("URL da imagem: " + urlIm);
+        }
+
+    }
+
+    private static String[] parse(String json) {
+        Matcher matcher = Pattern.compile(".*\\[(.*)\\].*").matcher(json);
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("no match in " + json);
+        }
+
+        String[] moviesArray = matcher.group(1).split("\\},\\{");
+        moviesArray[0] = moviesArray[0].substring(1);
+        //System.out.println(moviesArray[0]);
+        int last = moviesArray.length - 1;
+        String lastString = moviesArray[last];
+        moviesArray[last] = moviesArray[last].substring(0, lastString.length() - 1);
+        //System.out.println(moviesArray[last]);
+        return moviesArray;
+    }
+
+    private static List<String> parseAtrib(String[] moviesArray, int pos){
+        return Stream.of(moviesArray)
+                .map(e -> e.split("\",\"")[pos])
+                .map(e -> e.split(":\"")[1])
+                .map(e -> e.replaceAll("\"", ""))
+                .collect(Collectors.toList());
+    }
+
+    private static List<String> parseTitles(String[] moviesArray){
+        return parseAtrib(moviesArray,3);
+    }
+
+    private static List<String> parseUrlImages(String[] moviesArray){
+        return parseAtrib(moviesArray,5);
     }
 }
